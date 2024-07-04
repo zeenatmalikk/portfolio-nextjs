417ad68b93272ae7d24b75c1f54863137f77bf24
Zeenat <zeenatkm301@gmail.com>
2024-07-04 23:52:27 +0530

    made some changs in project

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,12 +38,14 @@ const Footer = (props: Props) => {
         </p>
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((profile) => (
-            <div
-              key={profile.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={profile.img} alt={""} width={20} height={20} />
-            </div>
+            <a href={profile.link} target={"_blank"}>
+              <div
+                key={profile.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={profile.img} alt={""} width={20} height={20} />
+              </div>
+            </a>
           ))}
         </div>
       </div>
