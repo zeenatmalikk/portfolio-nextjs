32f9f47f9c569ccca870f4e929fa8a9d1a1c9d25
Zeenat <zeenatkm301@gmail.com>
2024-07-05 18:19:00 +0530

    added apos

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,14 +26,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">
-          Where seamless functionality meets stunning design
+            Where seamless functionality meets stunning design
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transforming Concepts into seamless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-lg">
-          Hello, I'm Zeenat, your Mumbai-based maestro of front-end development, weaving web magic one dynamic site at a time
+            Hello, I&apos;m Zeenat, your Mumbai-based maestro of front-end
+            development, weaving web magic one dynamic site at a time
           </p>
           <a href="#about">
             <MagicButton
