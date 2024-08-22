e0c9c46e0c5534044563fd4056d4cf5abefff9a2
Zeenat <zeenatkm301@gmail.com>
2024-08-22 17:04:02 +0530

    phase btn changes

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -108,12 +108,14 @@ const Card = ({
 const AceternityIcon = ({ order }: { order: string }) => {
   return (
     <div>
-      <button className="relative  inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex text-2xl h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2 text-white backdrop-blur-3xl font-bold">
+      <button className="p-[3px] relative">
+        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-3xl" />
+        <div
+          className="px-5 sm:px-8 py-2  bg-black relative group transition duration-200 text-white hover:bg-transparent flex items-center gap-3  rounded-3xl"
+        >
           {order}
-        </span>
-      </button>{" "}
+        </div>
+      </button>
     </div>
   );
 };
